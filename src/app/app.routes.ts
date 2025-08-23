2f1a557e3e546a35d68f06782cd898be2146d250
Gamal Elshamy <gamalelshamy74@gmail.com>
2025-08-23 19:05:29 +0300

    navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './core/layouts/auth-layout/auth-layout.component';
+import { BlankLayoutComponent } from './core/layouts/blank-layout/blank-layout.component';
+import { LoginComponent } from './core/auth/login/login.component';
+import { RegisterComponent } from './core/auth/register/register.component';
+import { HomeComponent } from './features/home/home.component';
+import { DetailsComponent } from './features/details/details.component';
+import { NotfoundComponent } from './features/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    {
+        /* auth */
+        path: '',
+        component: AuthLayoutComponent, children: [
+            { path: 'login', component: LoginComponent, title: 'MyDoctor - Login' },
+            { path: 'register', component: RegisterComponent, title: 'MyDoctor - Register' }
+        ]
+    },
+    {
+        /* blank */
+        path: '',
+        component: BlankLayoutComponent, children: [
+            { path: 'home', component: HomeComponent, title: 'MyDoctor' },
+            { path: 'details', component: DetailsComponent, title: 'MyDoctor - Details' }
+        ]
+    },
+    { path: '**', component: NotfoundComponent, title: 'NotFound Page' }
+];
